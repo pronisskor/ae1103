60fb4f7d59163642ca6836b919ee197b7da8af68
pronisskor <163367282+pronisskor@users.noreply.github.com>
2024-03-14 10:48:08 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+# from dotenv import load_dotenv
+# load_dotenv()
+import streamlit as st
+from langchain_openai import ChatOpenAI
+
+
+chat_model = ChatOpenAI()
+
+st.title('AI English')
+
+content = st.text_input('공부할 주제를 회화와 단어 중에 선택해주세요.')
+
+if st.button('영어공부하기'):
+    with st.spinner('Wait for it...'):
+          result = chat_model.predict(content + "란 영어 단어 뜻을 간단히 설명해줘")
+          st.write(result)
+
